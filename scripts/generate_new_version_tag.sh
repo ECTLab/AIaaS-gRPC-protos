770215a0d00784a0994f49ba4c621c72bc3e0220
Mohammad Mahdi Malmasi <mmahdim@Mohammads-DivarMacBook-Pro.local>
2023-05-16 09:32:41 +0330

    new tag ci bugs fixed

--- scripts/generate_new_version_tag.sh
+++ scripts/generate_new_version_tag.sh
@@ -1,6 +1,14 @@
 #!/bin/bash
 
-TAGS=$(git tag --sort=-version:refname --merged master --list 'v[0-9]*.[0-9]*.[0-9]*')
+git checkout master
+git fetch --tags
+
+TAGS=$(git tag --sort=-version:refname --list 'v[0-9]*.[0-9]*.[0-9]*')
+echo "Tags: $TAGS"
+if [ -z "$TAGS" ]; then
+    echo "No tags found"
+    exit 1
+fi
 
 LAST_TAG=$(echo "$TAGS" | head -n1)
 
@@ -8,7 +16,7 @@ LAST_DIGIT=$(echo "$LAST_TAG" | awk -F '.' '{print $3}')
 NEXT_DIGIT=$((LAST_DIGIT + 1))
 
 NEW_TAG=$(echo "$LAST_TAG" | sed "s/\.[0-9]*$/.$NEXT_DIGIT/")
+echo "New tag: $NEW_TAG"
 
-git checkout master
 git tag "$NEW_TAG"
 git push origin "$NEW_TAG"
