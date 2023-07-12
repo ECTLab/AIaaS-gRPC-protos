66c58d4a3808473ef7c21f2ae1bc219f8ade30fd
Mohammad Mahdi Malmasi <58389844+MmahdiM79@users.noreply.github.com>
2023-07-12 07:19:46 +0330

    Fix   init   file of module (#33)
    * sub modules import part added
    
    * init file renamed
    
    * add_init_files script updated
    
    * add_init_files script bug fixed
    
    * generate_python_stubs command removed
    
    ---------
    
    Co-authored-by: Mohammad Mahdi Malmasi

--- scripts/add_init_files.sh
+++ scripts/add_init_files.sh
@@ -9,4 +9,4 @@ for SUBFOLDER in $SUBFOLDERS; do
 done
 
 touch "$OUTPUT_FOLDER/__init__.py"
-cat scripts/text_files/python_add_folder_path_code.txt > "$OUTPUT_FOLDER/__init__.py"
+cat scripts/text_files/AIaaS_interface_init_file.txt > "$OUTPUT_FOLDER/__init__.py"
